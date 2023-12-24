7205de0d3033c36e4fedb3bd7181bea80a244aa9
RonIsakov <ron.isakov@post.runi.ac.il>
2023-12-24 22:15:38 +0200

    addTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,17 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
-public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+
+class AddTwo {
+    public static void main(String[] args) {
+    //user iserting two ints num1,num2
+    int num1 = Integer.parseInt(args[0]);
+    int num2 = Integer.parseInt(args[1]);
+	
+	//printing % java AddTwo + num1 space num2 
+	//printing num1 + num2 = the sum of num1 and num 2
+	System.out.println( "% java AddTwo " + num1 + " " + num2);
+    System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+
+
+		
+		
+    }
 }
